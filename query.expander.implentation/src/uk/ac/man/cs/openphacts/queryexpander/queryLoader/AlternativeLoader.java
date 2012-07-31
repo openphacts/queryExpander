4345d5e5f543a9897e4048eb7a9623d89485c9d5
Christian-B <brenninc@cs.man.ac.uk>
2012-07-31 11:42:06 +0100

    fix bug in test case

--- query.expander.implentation/src/uk/ac/man/cs/openphacts/queryexpander/queryLoader/AlternativeLoader.java
+++ query.expander.implentation/src/uk/ac/man/cs/openphacts/queryexpander/queryLoader/AlternativeLoader.java
@@ -41,17 +41,23 @@ public class AlternativeLoader extends QueryCaseLoader{
                 "   ?obj1 ?obj2 ?obj3 ?obj4 ?obj5 \n" +
                 "WHERE {\n" +
                 "	GRAPH <http://www.example.org/graph1> {\n" +
-                "		OPTIONAL { ?replacedURI1 example:pred1 ?obj1 }\n" +
-                "       FiLTER (?replacedURI1 = bar:1 || ?replacedURI1 = bar:2 ||\n" +
-                "               ?replacedURI1 = foo:subj1) \n" +
-                "		OPTIONAL { ?replacedURI2 example:pred2 ?obj2 }\n" +
-                "       FiLTER (?replacedURI2 = bar:1 || ?replacedURI2 = bar:2 ||\n" +
-                "               ?replacedURI2 = foo:subj1) \n" +
+                "		OPTIONAL { \n" +
+                "           ?replacedURI1 example:pred1 ?obj1 \n" +
+                "           FiLTER (?replacedURI1 = bar:1 || ?replacedURI1 = bar:2 ||\n" +
+                "                   ?replacedURI1 = foo:subj1) \n" +
+                "       }\n" +
+                "		OPTIONAL { " +
+                "           ?replacedURI2 example:pred2 ?obj2 \n" +
+                "           FiLTER (?replacedURI2 = bar:1 || ?replacedURI2 = bar:2 ||\n" +
+                "                  ?replacedURI2 = foo:subj1) \n" +
+                "       }\n" +
                 "	}\n" +
                 "	GRAPH <http://www.example.org/graph2> {\n" +
-                "		OPTIONAL { ?replacedURI3 example:pred3 ?obj3 }\n" +
-                "       FiLTER (?replacedURI3 = bar:1 || ?replacedURI3 = bar:2 ||\n" +
-                "               ?replacedURI3 = foo:subj1) \n" +
+                "		OPTIONAL { " +
+                "           ?replacedURI3 example:pred3 ?obj3 \n" +
+                "           FiLTER (?replacedURI3 = bar:1 || ?replacedURI3 = bar:2 ||\n" +
+                "                   ?replacedURI3 = foo:subj1) \n" +
+                "       }\n" +
                 "		?replacedURI4 example:pred4 ?obj4.\n" +
                 "       FiLTER (?replacedURI4 = bar:1 || ?replacedURI4 = bar:2 ||\n" +
                 "               ?replacedURI4 = foo:subj1) \n" +
