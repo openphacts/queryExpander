addc7f60c179a810a158471717e76187f4bdede9
Egon Willighagen <egon.willighagen@gmail.com>
2012-10-31 13:25:24 +0100

    Typo fix: fro -> from.

--- query.expander.ws.server/src/uk/ac/man/cs/openphacts/queryexpander/QueryExpanderWsServer.java
+++ query.expander.ws.server/src/uk/ac/man/cs/openphacts/queryexpander/QueryExpanderWsServer.java
@@ -675,7 +675,7 @@ public class QueryExpanderWsServer {
             sb.append(inputURI);
             sb.append("</h2>\n"); 
             if (graph != null && !graph.isEmpty()){
-                sb.append("<h3>Limited to ones fro graph ");
+                sb.append("<h3>Limited to ones from graph ");
                 sb.append(graph);
                 sb.append("</h3>\n");     
             }
