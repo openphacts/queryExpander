f43564036025814053fb575f6868b47806064b3e
Christian-B <brenninc@cs.man.ac.uk>
2012-11-21 12:20:49 +0000

    Change default service name

--- query.expander.ws.server/src/uk/ac/man/cs/openphacts/queryexpander/QueryExpanderWsServer.java
+++ query.expander.ws.server/src/uk/ac/man/cs/openphacts/queryexpander/QueryExpanderWsServer.java
@@ -43,6 +43,10 @@ public class QueryExpanderWsServer extends WSLinksetService{
         queryExpander = new QueryExpanderImpl(imsMapper);
     }
     
+    @Override
+    public String getDefaultResourceName(){
+        return "QueryExpander";
+    }
 /*    private final String HEADER_START = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
             + "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
             + "<html xmlns:v=\"urn:schemas-microsoft-com:vml\">\n"
